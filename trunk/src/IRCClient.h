a958443436c785149f67347ca5b82556844f10f1
death420.x <death420.x@dfcbb000-c142-0410-b1aa-f54c88fa44bd>
2008-01-02 19:47:37 +0000

    Ok This Is The First Commit On The New SVN, I Have Not Updated MangChat Since Last Forum Post.. So I Will Just Put In The Last Entry From The Changelog..
    *** MangChat1.01 Compatible With MaNGOS 4955+ ***
    1) Combined Sql Files Into One File (MangChat.sql)
    2) Added Shutdown Command
    3) Added Option To See InGame Announces/Notifies In IRC
    4) Fixed UnMute Problem With Player In Game During release
    5) Fixed player Command (Guild, Money, XP)
    6) Cleaned Up Some GCC Compile Warnings

---
+++ trunk/src/IRCClient.h
@@ -0,0 +1,203 @@
+#ifndef _IRC_CLIENT_H
+#define _IRC_CLIENT_H
+
+#include "Policies/Singleton.h"
+#include "Player.h"
+
+using namespace std;
+// The maximum ammount of channels used
+// in the channel array you can have as much channels as you
+// want, but it is important to always have at least equal or more
+// channels then you specify in your mangosd.conf
+#define MAX_CONF_CHANNELS 10
+#define MAX_CHAT_LINES 10
+// time we need to wait before we try another connecton attempt
+// Default is 30 seconds
+#define WAIT_CONNECT_TIME sIRC._wct
+#define MAX_SCRIPT_INST 10
+// CLINES is used for the default chatlines
+// By using the GetChatLine function its easier and faster
+// to receieve the line you need.
+enum CLINES
+{
+	IRC_WOW = 0,
+	WOW_IRC = 1,
+	JOIN_WOW = 2,
+	JOIN_IRC = 3,
+	LEAVE_WOW = 4,
+	LEAVE_IRC = 5,
+	CHANGE_NICK = 6
+}; // Chatlines
+// CACTION is used by the Handle_WoW_Channel function
+// this function is called in channel.h when a player
+// joins or leave a channel inside the client.
+enum CACTION
+{
+	CHANNEL_JOIN,
+	CHANNEL_LEAVE,
+};
+enum CIMSG
+{
+	MSG_PRIV = 0,
+	MSG_NOTICE = 1,
+	MSG_ACTION = 2,
+};
+// IRCClient main class
+class IRCClient : public ZThread::Runnable
+{
+public:
+	// IRCClient Constructor
+        IRCClient();
+	// IRCClient Destructor
+        ~IRCClient();
+	// ZThread Entry
+        void run();
+public:
+	// IRCClient active
+	bool	Active;
+	// Connected to IRC
+	bool	Connected;
+	// Socket indentifier
+	int		SOCKET;
+	fd_set	sfdset;
+	// Send data to IRC, in addition the endline is added \n
+	bool	SendIRC(std::string data);
+	// This function is called in ChatHandler.cpp and processes the chat from game to IRC
+	void	Send_WoW_IRC(Player *plr, std::string Channel, std::string Msg);
+	// Sends a message to all players on the specified channel
+	void	Send_WoW_Channel(const char *channel, std::string chat);
+	// Send a system message to all players
+	void	Send_WoW_System(std::string Message);
+	// Send a message to the specified IRC channel
+	void	Send_IRC_Channel(std::string sChannel, std::string sMsg, bool NoPrefix = false, int nType = MSG_PRIV);
+	// Sends a message to all IRC Channels
+	void	Send_IRC_Channels(std::string sMsg);
+	std::string MakeMsg(std::string msg, std::string var, std::string val)
+	{
+	    std::size_t start = msg.find(var);
+	    if (start != std::string::npos)
+			msg.replace(start, var.length(), val);
+		return msg;
+	}
+	void    Send_WoW_Player(string sPlayer, string sMsg);
+	void    Send_WoW_Player(Player *plr, string sMsg);
+
+	// This function is called in Channel.cpp and processes Join/leave messages
+	void	Handle_WoW_Channel(std::string Channel, Player *plr, int nAction);
+	void ResetIRC();
+public:
+	// IRC Server host
+	string	_Host;
+	// IRC Server Port
+	int	_Port;
+	// IRC Username
+	string	_User;
+	// IRC Password
+	string	_Pass;
+	// IRC Nickname
+	string	_Nick;
+	// Authentication type
+	int	_Auth;
+	// IRC Connect code
+	string	_ICC;
+	// IRC Default channel
+	string	_defchan;
+	// IRC Leave Default channel
+	int	_ldefc;
+	// Wait Connect Time
+	int _wct;
+	// BotMask Options
+	int Botmask;
+	// IRC Channel count
+	// DO NOT CHANGE THIS
+	// if you wish to handle more then 10 channes
+	// change MAX_CONF_CHANNELS from the defines.
+	int	_chan_count;
+	// IRC Channel list
+	// Array to store our IRC channels
+	// each element will corrospond
+	// with _wow_chan array below.
+	string	_irc_chan[MAX_CONF_CHANNELS];
+	// Game Channel list
+	string	_wow_chan[MAX_CONF_CHANNELS];
+        // Channel OnJoin/Restart/Kick Messages
+        string  JoinMsg;
+        string  RstMsg;
+		string  kikmsg;
+		// Misc Options
+		string  ojGM1;
+		string  ojGM2;
+		string  ojGM3;
+		string  ojGM4;
+		string  ojGM5;
+        // IRC Commands Security Level
+	    int		CFUN;
+        int		CHELP;
+		int     CITEM;
+        int     CJAIL;
+        int     CKICK;
+        int     _KILL;
+        int     CLEVEL;
+		int     CMONEY;
+        int     CMUTE;
+        int     CPLAYER;
+        int     CPM;
+        int     CRESTART;
+        int     CREVIVE;
+		int		CSAVEALL;
+		int		CSHUTDOWN;
+        int     CSPELL;
+        int     CSYSMSG;
+        int     CTELE;
+        int     CWHO;
+        int     CZBUFF;
+		// ZBuff Options
+		int     ZBUFF_NPC;
+		int		ZBUFF_ANIM;
+		int		ZBUFF_SPELL1;
+		int		ZBUFF_SPELL2;
+		int		ZBUFF_SPELL3;
+		int		HBUFF_SPELL1;
+		int		HBUFF_SPELL2;
+		int		HBUFF_SPELL3;
+        // BotMask
+        int     BOTMASK;
+	// Max connect attempt
+	int     _MCA;
+	// Auto rejoin when kicked from irc
+	int     _autojoinkick;
+	// IRC Command prefix
+	string  _cmd_prefx;
+	// contains the Chatlines
+	// Array that contains our chatlines from the conf file
+	// To increase this value change the MAX_CHAT_LINE define above
+	// Make sure the number of elements must match your items
+	// (remeber this starts at 0 so 0..9 is 10 items)
+	// and that you load the line in the LoadConfig function.
+	string	ILINES[MAX_CHAT_LINES];
+	string	GetChatLine(int nItem);
+
+	int _Max_Script_Inst;
+	// MAX_SCRIPT_INST
+private:
+	// Load configuration from the mangosd.conf file
+	// to be used with the irc client.
+	bool	LoadConfig();
+	// Returns default chatline based on enum CLINES
+	// Initialize socket library
+	bool	InitSock();
+	// Connect to IRC Server
+	bool	Connect(const char *cHost, int nPort);
+	// Login to IRC Server
+	bool	Login(std::string sNick, std::string sUser, std::string sPass);
+	// Send raw data to IRC
+	bool	SendData(const char *data);
+	// Disconnect from IRC and cleanup socket
+	void	Disconnect();
+	// Processes the data receieved from IRC
+	void	Handle_IRC(std::string sData);
+	// Receieves data from the socket.
+	void	SockRecv();
+};
+#define sIRC MaNGOS::Singleton<IRCClient>::Instance()
+#endif
---
+++ trunk/src/IRCCmd.h
@@ -0,0 +1,84 @@
+#ifndef _IRC_CMD_H
+#define _IRC_CMD_H
+#define MAX_CLIENTS 50
+#include "Common.h"
+#include "Player.h"
+#include "ObjectAccessor.h"
+struct _client
+{
+	bool		LoggedIn;
+	std::string Name;
+	std::string UName;
+	int		GMLevel;
+};
+struct _CDATA
+{
+	std::string CMD;
+	std::string USER;
+	std::string FROM;
+	std::string PARAMS;
+	int PCOUNT;
+};
+enum APVERR
+{
+	E_OK,
+	E_SIZE,
+	E_AUTH,
+	E_IVALID,
+};
+enum ESOUNDS
+{
+	S_ENTERWORLD	        = 602,
+	S_QUESTFAILED	        = 847,
+	S_INVITE		= 880,
+	S_LEVELUP		= 888,
+	S_COINSOUND		= 895,
+	S_WHISPER		= 3081,
+	S_STEALTH		= 3325,
+};
+class IRCCmd
+{
+public:
+        IRCCmd();
+        ~IRCCmd();
+	bool	IsValid(std::string USER, std::string FROM, std::string CHAT);
+	void	Handle_Logout(_CDATA *CD);
+	bool	IsLoggedIn(std::string USER);
+private:
+	void	Handle_Login(_CDATA *CD);
+	void	Online_Players(_CDATA *CD);
+	int	GetLevel(std::string sName);
+        // InGame Commands
+	void	Fun_Player(_CDATA *CD);
+	void	Help_IRC(_CDATA *CD);
+    void	Item_Player(_CDATA *CD);
+	void	Inchan_Server(_CDATA *CD);
+    void	Info_Server(_CDATA *CD);
+	void	Jail_Player(_CDATA *CD);
+	void	Kick_Player(_CDATA *CD);
+	void	Kill_Player(_CDATA *CD);
+	void	Level_Player(_CDATA *CD);
+	void	Money_Player(_CDATA *CD);
+	void	Mute_Player(_CDATA *CD);
+	void	Player_Info(_CDATA *CD);
+	void	PM_Player(_CDATA *CD);
+	void	Revive_Player(_CDATA *CD);
+	void	Saveall_Player(_CDATA *CD);
+	void	Shutdown_Mangos(_CDATA *CD);
+	void	Spell_Player(_CDATA *CD);
+    void	Sysmsg_Server(_CDATA *CD);
+    void	Tele_Player(_CDATA *CD);
+	void	Who_Logged(_CDATA *CD);
+	void	Zbuff_Player(_CDATA *CD);
+	bool    CanUse(std::string USER, int nLevel);
+	bool    ValidParams(std::string PARAMS, int nCount = 1);
+	bool    ParamsValid(_CDATA *CD, int pCnt);
+	int     ParamsValid(_CDATA *CD, int pCnt, int rLev);
+	Player* GetPlayer(std::string WHO);
+    std::string GetAccName(std::string sName);
+    std::string ChanOrPM(_CDATA *CD);
+	std::string* getArray(std::string PARAMS, int nCount = 1);
+	std::string MakeMsg(const char *sLine, ... );
+	_client CLIENTS[MAX_CLIENTS];
+};
+#endif
